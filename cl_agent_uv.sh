cf45168c2c358e7a1807f5946768f35314515789
Serg <s.lungullo@gmail.com>
2026-01-08 14:44:00 +0100

    feat: add the current barnch checking in `cl_agent_uv.sh`

--- cl_agent_uv.sh
+++ cl_agent_uv.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-cd app
-uv run streamlit run streamlit_app.py
+# Store the current branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$1" == "--remote" || "$1" == "-r" ]]; then
+    # Remote mode
+    if [ "$current_branch" == "master" ]; then
+        echo "Switching to deploy_gcp..."
+        git checkout deploy_gcp || exit 1
+    fi
+
+    cd app
+    uv run streamlit run streamlit_vrtx.py
+else
+    # Local mode
+    if [ "$current_branch" == "deploy_gcp" ]; then
+        echo "Switching to master..."
+        git checkout master || exit 1
+    fi
+
+    cd app
+    uv run streamlit run streamlit_app.py
+fi
